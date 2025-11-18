4a8989ed32efa60864a1f3d51c190926eaf1e972
Jiwoo Ahn <ilp_rincipe@naver.com>
2025-11-18 14:15:44 +0900

    Add KallGraph build

---
+++ build/src/CMakeFiles/KallGraph.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jiwoo/goinfre/KallGraph/src/KallGraph.cpp" "src/CMakeFiles/KallGraph.dir/KallGraph.cpp.o" "gcc" "src/CMakeFiles/KallGraph.dir/KallGraph.cpp.o.d"
+  "/home/jiwoo/goinfre/KallGraph/src/lib/KallGraphAlgo.cpp" "src/CMakeFiles/KallGraph.dir/lib/KallGraphAlgo.cpp.o" "gcc" "src/CMakeFiles/KallGraph.dir/lib/KallGraphAlgo.cpp.o.d"
+  "/home/jiwoo/goinfre/KallGraph/src/lib/Util.cpp" "src/CMakeFiles/KallGraph.dir/lib/Util.cpp.o" "gcc" "src/CMakeFiles/KallGraph.dir/lib/Util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/KallGraph.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/KallGraph"
+  "../bin/KallGraph.pdb"
+  "CMakeFiles/KallGraph.dir/KallGraph.cpp.o"
+  "CMakeFiles/KallGraph.dir/KallGraph.cpp.o.d"
+  "CMakeFiles/KallGraph.dir/lib/KallGraphAlgo.cpp.o"
+  "CMakeFiles/KallGraph.dir/lib/KallGraphAlgo.cpp.o.d"
+  "CMakeFiles/KallGraph.dir/lib/Util.cpp.o"
+  "CMakeFiles/KallGraph.dir/lib/Util.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KallGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
